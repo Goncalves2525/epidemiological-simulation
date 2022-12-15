22fc7ef8c81078af43849dd9c48a3fa5754c1394
Luis Leal <1100253@isep.ipp.pt>
2022-12-15 20:24:58 +0000

    alteração 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("babanas locas!");
+        System.out.println("babanas locas nas as!");
 
         System.out.println("Teste");
         System.out.println("teste2");
         System.out.println("Teste");
+        System.out.println("lolo");
     }
 
 
