89b01bff103e16f06595c56c95d73e0bcec203a8
1212039 <1212039@isep.ipp.pt>
2022-12-22 18:50:00 +0000

    correção formatação string

--- src/Calculos.java
+++ src/Calculos.java
@@ -1,6 +1,6 @@
 public class Calculos {
 
-    public double derivadaROrdemT(double gama, double i, double alfa, double r, double ro, double beta, double s) {
+    public static double derivadaROrdemT(double gama, double i, double alfa, double r, double ro, double beta, double s) {
         return gama * i - alfa * r + (1 - ro) * beta * s * i;
     }
 }
